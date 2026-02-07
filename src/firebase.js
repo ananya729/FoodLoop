ef60bc8e14903ff2ad4f6474e7ccdd7d92f672aa
Ananya Jaiswal <ananyajaiswal@MacBookAir.ht.home>
2026-02-07 13:48:13 -0600

    Use Firebase env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnLruiT0J_gPFKRVKq9VVC6nx_iqlPjwg",
-  authDomain: "foodloop-f7d60.firebaseapp.com",
-  projectId: "foodloop-f7d60",
-  storageBucket: "foodloop-f7d60.firebasestorage.app",
-  messagingSenderId: "402279630425",
-  appId: "1:402279630425:web:bc65a80e8b487508181348"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 
